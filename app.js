07686275b178384e5a6cf89f4e5e495b26f5d13f
dev_mikeyy <161330407+devmikeyy@users.noreply.github.com>
2024-04-19 20:03:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let left_btn = document.getElementsByClassName('bi-caret-left-fill')[0];
 let right_btn = document.getElementsByClassName('bi-caret-right-fill')[0];
 let cards = document.getElementsByClassName('cards')[0];
+let search = document.getElementsByClassName('search')[0];
 
 left_btn.addEventListener('click', ()=> {
     cards.scrollLeft -= 240; 
@@ -8,7 +9,6 @@ left_btn.addEventListener('click', ()=> {
  right_btn.addEventListener('click', ()=> {
     cards.scrollLeft += 240; 
  })
-
  let json_url = "anime.json"
 
  fetch(json_url).then(Response => Response.json())
@@ -33,4 +33,67 @@ left_btn.addEventListener('click', ()=> {
          `
          cards.appendChild(card);
     });
- });
+
+    document.getElementById('title').innerText = data[0].name;
+    document.getElementById('desc').innerText = data[0].desc;
+    document.getElementById('gen').innerText = data[0].genre;
+    document.getElementById('date').innerText = data[0].running;
+    document.getElementById('rate').innerHTML = `<span>MAL</span><i class="bi bi-star-fill"></i> ${data[0].mal}`;
+    
+    data.forEach(element => {
+        let {name, mal, running, sposter, genre, url} = element;
+        let card = document.createElement('a');
+        card.classList.add('card');
+        card.href = url;
+        card.innerHTML = `
+            <img src="${sposter}" alt="poster">
+            <div class="cont">
+                <h3>${name}</h3>
+                <p>${genre}, ${running},<span>MAL</span><i class="bi bi-star-fill">${mal}</i></p>
+            </div>
+        `
+        search.appendChild(card) // Append the card element, not the string 'card'
+    });
+    
+    search_input.addEventListener('keyup', () => {
+        let filter = search_input.value.toLowerCase();
+        let a = search.getElementsByTagName('a');
+      
+        if (filter === '') {
+          search.style.visibility = "hidden";
+          search.style.opacity = 0;
+          for (let index = 0; index < a.length; index++) {
+            a[index].style.display = "none";
+          }
+        } else {
+          search.style.visibility = "visible";
+          search.style.opacity = 1;
+          for (let index = 0; index < a.length; index++){
+            let b = a[index].getElementsByClassName('cont')[0];
+            let TextValue = b.textContent || b.innerText;
+            if (TextValue.toLowerCase().indexOf(filter) > -1) {
+              a[index].style.display = "flex";
+            } else {
+              a[index].style.display = "none";
+            }
+          }
+        }
+      });
+
+      let video = document.getElementsByTagName('video')[0];
+      let play = document.getElementById('play');
+    /*play.addEventListener('click', () => {
+       if(video.paused) {
+         video.play();
+         play.innerHTML = `PLAY <i class="bi bi-pause-fill"></i>`
+      } else {
+         video.pause();
+         play.innerHTML = `WATCH <i class="bi bi-play-fill"></i>`
+      }
+    });*/
+
+       
+       
+      
+         
+       });
